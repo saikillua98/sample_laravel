99d1cbeeb3c3b2eec7120a7703df4e3647d5fe88
Frank Lu <289803142@qq.com>
2018-07-16 10:12:48 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@
 
     use App\Models\User;
     use Illuminate\Http\Request;
+    use Auth;
 
     class UsersController extends Controller
     {
@@ -29,6 +30,7 @@
                 'email' => $request->email,
                 'password' => bcrypt($request->password),
             ]);
+            Auth::login($user);
             session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
             return redirect()->route('users.show', [$user]);
         }
